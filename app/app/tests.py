9891191c1ccbf217e321567ecd5381e1b658f918
laqin3 <laqin3@gmail.com>
2025-06-28 10:39:54 -0400

    Section7: Test Driven Development with Django

---
+++ app/app/tests.py
@@ -0,0 +1,38 @@
+"""
+Sample Test
+"""
+from django.test import SimpleTestCase
+from app import calc
+
+
+# 28. Write a test
+class ViewTest(SimpleTestCase):
+    def test_add(self):
+        """ Test adding numbers together."""
+        res = calc.add(5,6)
+
+        self.assertEqual(res,11)
+
+# 29. Write a test using TDD
+    """
+    In TDD: 
+        1. Write test for behaviour expected to see in code
+        2. Test fails
+        3. Write code so test passes
+
+        Use commande =>  docker compose run -rm app sh -c "python manage.py test" 
+    """
+
+    def test_subtraction(self):
+        """ Testing subtracting numbers """
+        res = calc.subtract(10, 15)
+
+        self.assertEqual(res,5)
+
+    # from rest_framework import APIClient
+    # def test_get_greetings(self):
+    #     """ Testing getting greeting """
+    #     client = APIClient()
+    #     res  = client.get('/greeting/')
+
+    #     self.assertEqual(res, "Hello!")
