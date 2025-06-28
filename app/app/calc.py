5c78168df1e2e542c5a19db59e5907d3f6990e91
laqin3 <laqin3@gmail.com>
2025-06-28 10:47:51 -0400

    fixing linting for calc.py tests.py

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,10 +2,10 @@
 Calculator functions
 """
 
-def add(x,y):
+def add(x, y):
     """ add x and y to return result."""
     return x + y
 
-def subtract(x,y):
+def subtract(x, y):
     """ subtract x from y and return result. """
     return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -9,30 +9,30 @@ from app import calc
 class ViewTest(SimpleTestCase):
     def test_add(self):
         """ Test adding numbers together."""
-        res = calc.add(5,6)
+        res = calc.add(5, 6)
 
-        self.assertEqual(res,11)
+        self.assertEqual(res, 11)
 
 # 29. Write a test using TDD
     """
-    In TDD: 
+        In TDD: 
         1. Write test for behaviour expected to see in code
         2. Test fails
         3. Write code so test passes
 
-        Use commande =>  docker compose run -rm app sh -c "python manage.py test" 
+        Use commande =>  
+        docker compose run -rm app sh -c "python manage.py test" 
     """
 
     def test_subtraction(self):
         """ Testing subtracting numbers """
         res = calc.subtract(10, 15)
 
-        self.assertEqual(res,5)
+        self.assertEqual(res, 5)
 
     # from rest_framework import APIClient
     # def test_get_greetings(self):
     #     """ Testing getting greeting """
     #     client = APIClient()
     #     res  = client.get('/greeting/')
-
     #     self.assertEqual(res, "Hello!")
