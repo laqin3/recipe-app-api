5c6bec75f587cb187dc4c31d9bad6665193e6a0a
laqin3 <laqin3@gmail.com>
2025-07-07 10:17:03 -0400

    Section13 part1 fixing views method name

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -11,4 +11,4 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
-        read_only_fields = ['id']
+        read_only_fields = ('id',)
